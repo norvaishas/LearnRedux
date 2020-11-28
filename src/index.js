0700c5fa53d6e1892971e2918e0af5209270264f
Sergei Norvaishas <snorvaishas@yandex.ru>
2020-11-28 05:05:38 +0300

    use action with parameter

--- src/index.js
+++ src/index.js
@@ -13,6 +13,9 @@ const reducer = (state = 0, action) => {
         case 'DEC':
             return state - 1;
 
+        case 'RND':
+            return state + action.payload;
+
         // Если значение из action.type не распознано, вернуть state без изменений
         default:
             return state;
@@ -25,13 +28,23 @@ document
   .getElementById('inc')
   .addEventListener('click', () => {
     store.dispatch( {type: 'INC'});
-})
+});
 
 document
   .getElementById('dec')
   .addEventListener('click', () => {
     store.dispatch( {type: 'DEC'});
-})
+});
+
+document
+  .getElementById('rnd')
+  .addEventListener('click', () => {
+      const payload = Math.floor(Math.random() * 10);
+      store.dispatch( {
+          type: 'RND',
+          payload
+      });
+  });
 
 const update = () => {
     document.getElementById('counter')
