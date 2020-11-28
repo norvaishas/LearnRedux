1cee02af3b690a76b7f12c7662408334579aed52
Sergei Norvaishas <snorvaishas@yandex.ru>
2020-11-28 03:54:23 +0300

    create reducer and action

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('Learn Redux')
+
+const reducer = (state = 0, action) => {
+    // state - текущий стейт
+    // action - действие выполняемое над стейтом
+    if (action.type === 'INC') {
+        return state + 1;
+    }
+    switch (action.type) {
+        case 'INC':
+            return state + 1;
+
+        // Если значение из action.type не распознано, вернуть state без изменений
+        default:
+            return state;
+    }
+};
+
+let state = reducer( undefined, {} ); // Инициализация стейта - action специально пустой чтоб попасть в default
+state = reducer(state, { type: 'INC' });
+console.log(state);
+
+state = reducer(state, { type: 'INC' });
+console.log(state);
