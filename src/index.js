01e465a27c874573b6636d2a3683683d7f1bd425
Sergei Norvaishas <snorvaishas@yandex.ru>
2020-11-29 03:28:12 +0300

    refactor: start use react (add react-component and move UI inside it)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import {createStore, bindActionCreators} from 'redux';
 import reducer from './reducer';
 import * as actions from './actions';
+import Counter from './counter';
 
 const store = createStore(reducer);
 const { dispatch } = store;
@@ -8,24 +11,18 @@ const { dispatch } = store;
 // Теперь функции в объекте будут называться так же как оригинальные экшенКриэйтеры из файла actions.js
 const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
 
-document
-  .getElementById('inc')
-  .addEventListener('click', inc);
-
-document
-  .getElementById('dec')
-  .addEventListener('click', dec);
-
-document
-  .getElementById('rnd')
-  .addEventListener('click', () => {
-      const payload = Math.floor(Math.random() * 10);
-      rnd(payload);
-  });
-
 const update = () => {
-    document.getElementById('counter')
-      .innerHTML = store.getState();
+    ReactDOM.render(
+      <Counter
+        counter={store.getState()}
+        inc={inc}
+        dec={dec}
+        rnd={() => {
+            const value = Math.floor(Math.random() * 10 );
+            rnd(value);
+        }}/>,
+      document.getElementById('root'));
 }
 
+update();
 store.subscribe(update);
